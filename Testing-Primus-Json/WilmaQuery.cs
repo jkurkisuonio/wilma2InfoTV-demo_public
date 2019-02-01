22de3f863c3fb0e8f84a7ed9424f56e3fcd88c35
Jani Kurki-Suonio <jani.kurki-suonio@edupoli.fi>
2019-02-01 16:15:20 +0200

    kommentit

--- Testing-Primus-Json/WilmaQuery.cs
+++ Testing-Primus-Json/WilmaQuery.cs
@@ -40,7 +40,41 @@ namespace Testing_Primus_Json
                 WilmaClassResourse huoneenVarauksetTaltaVuodelta = JsonConvert.DeserializeObject<WilmaClassResourse>(result2);
                 foreach (RecourceSchedule varaukset in huoneenVarauksetTaltaVuodelta.RecourceSchedules)
                 {
-              
+                    // Tilan ominaisuudet on koodattu tähän Caption kenttään. Formaatti: 
+                    /*                     
+                Esimerkkejä AMT-ATK-1-C03-22
+                AMT-NEU-2-Neuvotteluhuone B-12
+                ASK-LII-1-Liikuntasali-0
+                HER-ERI-3-Hoiva-20
+                LOV-LUE-1-110-0
+                PMT-TEO-1-C11-18
+                POM-MUU-1-Iris-0
+                Paikkakunta ASK Askola
+                HER Helsinki
+                KER Kerava
+                LOV Loviisa 
+                AMT Porvoo, Ammattitie
+                PMT Porvoo, Perämiehentie
+                POM Porvoo, POMO-talo
+                VAN Vantaa
+                Tilatyyppi ATK Atk-luokka
+                ERI Käytännön opetustilat, erikoisluokka
+                KIE Kielistudio
+                LII Liikuntatila
+                LUE Luentotila
+                MUU Muu tila
+                NEU Neuvotteluhuone
+                TEO Teorialuokka
+                YKS Yksilöohjaustila
+                Kerros 1 1. kerros
+                2
+                3
+                4
+                5
+                Tilan nimi Virna Tilan nimi
+                C06
+                Mega
+                Kapasiteetti 36 Tilaan mahtuma henkilömäärä*/
                     var nimi = varaukset.Schedule.FirstOrDefault()?.Groups.FirstOrDefault()?.Rooms.FirstOrDefault()?.Caption;
                     roomlist.Add(i, nimi);
                     CareeriaRoom careeriaRoom = CareeriaSpesific.ParseRoom(i, nimi);
